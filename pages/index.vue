e3577dececff32ebf722693287fd91d632c16e80
clemcodes <clemcodes@outlook.com>
2022-11-27 12:47:38 +0000

    utilities tryout

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <body class="bg-green-400">
-    <h1>Hello</h1>
+  <body class="bg-gray-100 sm:bg-yellow-300">
+    <div class="px-8 py-12 max-w-md mx-auto">
+      <img class="h-25" src="../assets/logo.png" alt="Prince's Trust, Youth Can Do It">
+      <img class="mt-6 rounded-lg shadow-xl" src="../assets/young-people.jpg" alt="a group of young people">
+      <h1 class="mt-6 text-2xl font-bold text-gray-900">Try New Things. <span class="text-red-600">Unlock your potential.</span></h1>
+      <p class="mt-2 text-gray-600">Our personal development programmes will help develop your confidence. Try new things, meet new people and develop essential skills at the same time.</p>
+      <div class="mt-4">
+        <a class="bg-red-500 px-5 py-3 inline-block rounded-lg shadow-lg text-white uppercase tracking-wider font-semibold text-sm" href="#">Book your first step</a>
+      </div>
+    </div>   
+    
   </body>
 </template>
 
