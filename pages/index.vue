35ce34628c97c0c1c9cb871b50f018744f6bb4d3
clemcodes <clemcodes@outlook.com>
2022-11-27 13:28:04 +0000

    responsive on smaller device

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <body class="bg-gray-100 sm:bg-yellow-300">
-    <div class="px-8 py-12 max-w-md mx-auto">
+  <body class="bg-gray-100">
+    <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl">
       <img class="h-25" src="../assets/logo.png" alt="Prince's Trust, Youth Can Do It">
-      <img class="mt-6 rounded-lg shadow-xl" src="../assets/young-people.jpg" alt="a group of young people">
-      <h1 class="mt-6 text-2xl font-bold text-gray-900">Try New Things. <span class="text-red-600">Unlock your potential.</span></h1>
-      <p class="mt-2 text-gray-600">Our personal development programmes will help develop your confidence. Try new things, meet new people and develop essential skills at the same time.</p>
+      <img class="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-96 sm:w-full sm:object-cover object-center" src="../assets/young-people.jpg" alt="a group of young people">
+      <h1 class="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl">Try New Things. <span class="text-red-600">Unlock your potential.</span></h1>
+      <p class="mt-2 text-gray-600 sm:mt-4 sm:text-xl">Our personal development programmes will help develop your confidence. Try new things, meet new people and develop essential skills at the same time.</p>
       <div class="mt-4">
-        <a class="bg-red-500 px-5 py-3 inline-block rounded-lg shadow-lg text-white uppercase tracking-wider font-semibold text-sm" href="#">Book your first step</a>
+        <a class="bg-red-500 px-5 py-3 inline-block rounded-lg shadow-lg text-white uppercase tracking-wider font-semibold text-sm sm:text-base" href="#">Book your first step</a>
       </div>
     </div>   
     
